8d6e261b7137bcf18ca809759d5d26cf81129cca
Hafizh <hafizhhugo@gmail.com>
2024-07-04 08:30:58 +0700

    cypress

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,27 +1,27 @@
-import "./login.scss"; 
+import "./login.scss";
 import { useContext, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-const Login = () => { 
+const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
-  const {dispatch} = useContext(AuthContext);
+  const { dispatch } = useContext(AuthContext);
   const handleLogin = (e) => {
     e.preventDefault();
 
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user; 
+        const user = userCredential.user;
         //console.log(user);
-        dispatch({type:"LOGIN",payload:user});
-        navigate('/')
+        dispatch({ type: "LOGIN", payload: user });
+        navigate("/");
       })
       .catch(() => {
         setError(true);
@@ -30,18 +30,22 @@ const Login = () => {
 
   return (
     <div className="login">
-      <form onSubmit={handleLogin}>
+      <form data-testid="form" onSubmit={handleLogin}>
         <input
+          id="email"
           type="email"
-          placeholder="email" 
+          placeholder="Enter your email"
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
+          id="password"
           type="password"
-          placeholder="password" 
+          placeholder="Enter your password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <button type="submit" data-testid="submit">
+          Login
+        </button>
         {error && <span>Wrong email or password!</span>}
       </form>
     </div>
